6b9d639142bf89e56826b16f88f9c9c0f6c6538d
SonikPixelz901 <124068508+SonikPixelz901@users.noreply.github.com>
2025-05-23 19:36:20 +0300

    Create mainwindow.cpp

---
+++ src/mainwindow.cpp
@@ -0,0 +1,11 @@
+#include "mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    setWindowTitle("Aerostrix Browser");
+    resize(1024, 768);
+    // Setup UI components later here
+}
+
+MainWindow::~MainWindow() {}
